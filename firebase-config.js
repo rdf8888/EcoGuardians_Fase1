efd3cf3f0f18f98f6556be13d560d95d3a996c68
rdf8888 <rjvsb22@gmail.com>
2025-07-29 16:39:48 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,48 +1,7 @@
-// Configuração do Firebase - substitua com os dados reais do seu Firebase
+
+// Configuração fictícia do Firebase
 const firebaseConfig = {
   apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  authDomain: "seu-projeto.firebaseapp.com",
+  projectId: "eco-guardians",
 };
-
-// Inicializa o Firebase
-firebase.initializeApp(firebaseConfig);
-// Configuração do Firebase - substitua com os dados reais do seu Firebase
-const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
-};
-
-// Inicializa o Firebase
-firebase.initializeApp(firebaseConfig);
-// Configuração do Firebase - substitua com os dados reais do seu Firebase
-const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
-};
-
-// Inicializa o Firebase
-firebase.initializeApp(firebaseConfig);
-// Configuração do Firebase - substitua com os dados reais do seu Firebase
-const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
-};
-
-// Inicializa o Firebase
-firebase.initializeApp(firebaseConfig);
