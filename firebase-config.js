03b28f12576f3293fe609d500e8c4ef899b54b51
rdf8888 <rjvsb22@gmail.com>
2025-07-29 20:53:21 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,15 @@
+// Arquivo de configuração Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 
-// Configuração fictícia do Firebase
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "eco-guardians",
+  apiKey: "AIzaSyA-l9Ab-NmaETxLx5_R5jjX6weGNNhJA64",
+  authDomain: "ecoguardians-ecossistema.firebaseapp.com",
+  databaseURL: "https://eco-guarrdians-default-rtdb.firebaseio.com",
+  projectId: "ecoguardians-ecossistema",
+  storageBucket: "ecoguardians-ecossistema.firebasestorage.app",
+  messagingSenderId: "446449980812",
+  appId: "1:446449980812:web:54aee769918a2ebc4df2a9",
+  measurementId: "G-YEMGW4Y10W"
 };
+
+export const app = initializeApp(firebaseConfig);
