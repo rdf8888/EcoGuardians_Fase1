57f2ded9a1b8ba3c4ec817791b9d5e604b9eb1d9
rdf8888 <rjvsb22@gmail.com>
2025-07-31 23:55:33 -0300

    Delete config.js
    Deletado.  Foi atualizado

--- config.js
+++
@@ -1,5 +0,0 @@
-
-const GEMINI_API_KEY = "AIzaSyCdXfHDTXTbkTnV0FLZKsb5JhNmYFKJX-Y";
-const SUPABASE_URL = "https://ojetradaxgrqnmdolxjw.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qZXRyYWRheGdycW5tZG9seGp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3NDIyMDUsImV4cCI6MjA2OTMxODIwNX0.GRrCv8uOUczhHT3Bae643YgIrzX426n5LvvS0ht3WmA";
-const supabase = supabase_js.createClient(SUPABASE_URL, SUPABASE_KEY);
